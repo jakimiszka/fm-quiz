0fef6cb7dadd18caa55190b6413c08fe9ca4f4ed
jakimiszka <jarek.jakimicha@gmail.com>
2025-09-02 19:54:22 +0200

    light/dark mode added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('js loaded');
+const toggle = document.querySelector('input[type="checkbox"]');
+
+
+toggle.addEventListener('change', () => {
+  const theme = toggle.checked ? 'dark' : 'light';
+  console.log(theme);
+  document.body.setAttribute('data-theme', theme);
+});
