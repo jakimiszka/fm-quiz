8bc74d9637e94672303e5ec53db1c4d5b02972fc
jakimiszka <jarek.jakimicha@gmail.com>
2025-09-09 20:05:06 +0200

    refactored coreect/wrong selection

---
+++ js/dataStore.js
@@ -0,0 +1,69 @@
+class DataStore{
+    constructor(){
+        this.data = null;
+        this.category = '';
+        this.questions = [];
+        this.answers = [];
+        this.quiz_counter = 0;
+        this.question_amount = 0;
+        this.init();
+    }
+
+    init(){
+        fetch('../data.json')
+        .then(response => response.json())
+        .then(data => {
+            this.data = data;
+            console.log('DataStore initialized');
+        })
+        .catch(error => console.error(error));
+    }
+
+    getData(){
+        return this.data;
+    }
+
+    setCategory(category){
+        this.category = category;
+        return this.category;
+    }
+
+    getCategory(){
+        return this.category;
+    }
+
+    getCategoryQuestions(category){
+        return this.data.quizzes.filter(quiz => quiz.title === category)[0];
+    }
+
+    getQuestion(index){
+        return this.getCategoryQuestions(this.category).questions[index];
+    }
+
+    setQuestionAmount(amount){
+        this.question_amount = amount;
+    }
+
+    getCounter(){
+        return this.quiz_counter;
+    }
+
+    nextQuestion(){
+        if(this.quiz_counter < this.question_amount){
+            this.quiz_counter++;
+        }else{
+            this.resetCounter();
+        }
+        return this.quiz_counter;
+    }
+
+    resetCounter(){
+        this.quiz_counter = 0;
+    }
+
+    saveAnswer(answer){
+        this.answers.push(answer);
+    }
+}
+
+export default DataStore;
--- js/script.js
+++ js/script.js
@@ -27,64 +27,13 @@ const results_score = document.querySelector('.template-results-score');
 const score_number = document.querySelector('.score--number');
 // restart
 const restart_btn = document.querySelector('.restart_btn');
+// correct / wrong icon
+const correct_icon_path = '../assets/images/icon-correct.svg';
+const wrong_icon_path = '../assets/images/icon-error.svg';
 
-const DataStore = {
-  data: null,
-  questions: [],
-  category: '',
-  answers: [],
-  quiz_counter: 0,
-  question_amount: 0,
-
-  init() {
-    fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        this.data = data;
-    })
-    .catch(error => console.error(error));
-  },
-
-  getData() {
-    return this.data;
-  },
-  setCategory(category){
-    this.category = category;
-    return this.category;
-  },
-  getCategory(){
-    return this.category;
-  },
-  getCategoryQuestions(category){
-    return this.data.quizzes.filter(quiz => quiz.title === category)[0];
-  },
-  getQuestion(index){
-    return this.getCategoryQuestions(this.category).questions[index];
-  },
-  setQuestionAmount(amount){
-    this.question_amount = amount;
-  },
-
-  getCounter(){
-    return this.quiz_counter;
-  },
-  nextQuestion(){
-    if(this.quiz_counter <= this.question_amount){
-        this.quiz_counter++;
-    }else{
-        this.resetCounter();
-    }
-    return this.quiz_counter;
-  },
-  resetCounter(){
-    this.quiz_counter = 0;
-  },
-  saveAnswer(answer){
-    this.answers.push(answer);
-  }
-};
-
-DataStore.init();
+// DATA STORE
+import DataStore from './dataStore.js';
+const dataStore = new DataStore();
 
 function displayQuiz(){
     title_card.style.display = 'none';
@@ -99,7 +48,7 @@ function displayResults(score){
     options.style.display = 'none';
     results_title.style.display = 'flex';
     results_score.style.display = 'flex';
-    const quiz = DataStore.getCategoryQuestions(DataStore.getCategory());
+    const quiz = dataStore.getCategoryQuestions(dataStore.getCategory());
     console.log(quiz);
     updateHeader(quiz.icon, quiz.title);
     score_number.textContent = score;
@@ -142,11 +91,11 @@ function resetOptions(){
 // QUIZ CETEGORY
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        const category = DataStore.setCategory(btn.dataset.category);
-        const quiz = DataStore.getCategoryQuestions(category);
-        const question_index = DataStore.getCounter();
-        DataStore.setQuestionAmount(quiz.questions.length);
-        const question = DataStore.getQuestion(question_index);
+        const category = dataStore.setCategory(btn.dataset.category);
+        const quiz = dataStore.getCategoryQuestions(category);
+        const question_index = dataStore.getCounter();
+        dataStore.setQuestionAmount(quiz.questions.length);
+        const question = dataStore.getQuestion(question_index);
 
         updateHeader(quiz.icon, quiz.title);
         updateTitleAndQuiestion(question.question, question_index + 1);
@@ -177,8 +126,8 @@ toggle.addEventListener('change', () => {
 
 restart_btn.addEventListener('click', ()=>{
     //reset everything
-    DataStore.resetCounter();
-    DataStore.answers = [];
+    dataStore.resetCounter();
+    dataStore.answers = [];
     results_title.style.display = 'none';
     results_score.style.display = 'none';
     title_card.style.display = 'flex';
@@ -186,50 +135,56 @@ restart_btn.addEventListener('click', ()=>{
     title.style.display = 'none';
 });
 
-submit_btn.addEventListener('click', ()=>{
-    const question = DataStore.getQuestion(DataStore.getCounter());
-    const questionAnswer = question.answer;
-    const quiz_counter = DataStore.getCounter();
-    const answer_option = document.querySelector('.selected > .asnwer_option');
-    const selectedBtn = document.querySelector('.answer_btn.selected');
-    const answerContent = document.querySelector('.selected > .answer_content').textContent;
-    const answerIcon = document.querySelector('.selected > .answer_icon');
+// improve with first arg
+function handleCorrectAnswer(optionBtn, isSelected, isCorrect){
+    const selectedBtn = isSelected ? document.querySelector('.answer_btn.selected') : optionBtn;
+    const answer_option = selectedBtn.querySelector('.asnwer_option');
+    const answerIcon = selectedBtn.querySelector('.answer_icon');
 
-    if(quiz_counter >= DataStore.question_amount - 1){
-        console.log('quiz completed - ur score is ' + DataStore.answers.length);
-        displayResults(DataStore.answers.length);
+    if(isCorrect){
+        // show correct answer
+        answerIcon.src = '../assets/images/icon-correct.svg';
+        selectedBtn.classList.add('correct');
+        answer_option.classList.add('correct_option');
+        answerIcon.style.display = 'block';
+    }else{
+        // show wrong answer
+        answerIcon.src = '../assets/images/icon-error.svg';
+        selectedBtn.classList.add('wrong');
+        answer_option.classList.add('wrong_option');
+        answerIcon.style.display = 'block';
+    }
+}
+
+submit_btn.addEventListener('click', ()=>{
+    const question = dataStore.getQuestion(dataStore.getCounter());
+    const questionAnswer = question.answer;
+    const quiz_counter = dataStore.getCounter();
+    const answerContent = document.querySelector('.selected > .answer_content').textContent;
+
+    if(quiz_counter >= dataStore.question_amount - 1){
+        console.log('quiz completed - ur score is ' + dataStore.answers.length);
+        displayResults(dataStore.answers.length);
     }else{
         if(answerContent !== questionAnswer){
-            answerIcon.src = '../assets/images/icon-error.svg';
-            selectedBtn.classList.add('wrong');
-            console.log(answerIcon);
-            answer_option.classList.add('wrong_option');
-            answerIcon.style.display = 'block';
+            handleCorrectAnswer(null ,true, false);
 
-             answer_btns.forEach((option, index) => {
+             // find good answer and highlight it
+             answer_btns.forEach(option => {
                 const answerContent = option.querySelector('.answer_content').textContent;
                 if(answerContent === questionAnswer){
-                    const correctBtn = option.querySelector('.asnwer_option');
-                    console.log('1',correctBtn);
-                    const correctIcon = option.querySelector('.answer_icon');
-                    console.log('2',correctIcon);
-                    correctIcon.src = '../assets/images/icon-correct.svg';
-                    correctBtn.classList.add('correct_option');
-                    option.classList.add('correct');
-                    correctIcon.style.display = 'block';
+                    handleCorrectAnswer(option, false, true);
                 }
             });
         }else{
-            answerIcon.src = '../assets/images/icon-correct.svg';
-            selectedBtn.classList.add('correct');
-            DataStore.saveAnswer(true);
-            answer_option.classList.add('correct_option');
-            answerIcon.style.display = 'block';
+            dataStore.saveAnswer(true);
+            handleCorrectAnswer(null, true, true);
         }
 
+        //TODO: freeze options during timeout
         setTimeout(() => {
-            const question_index = DataStore.nextQuestion();
-            const question = DataStore.getQuestion(question_index)
+            const question_index = dataStore.nextQuestion();
+            const question = dataStore.getQuestion(question_index)
             updateTitleAndQuiestion(question.question, question_index + 1);
             updateOptions(question.options);
         }, 2000);
